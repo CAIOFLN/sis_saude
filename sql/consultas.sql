eb84edc7b96496d24aa0bdff5deb0a3f0156fc2d
Guilherme Sonego <137665368+GuilhermeSonego@users.noreply.github.com>
2025-11-21 22:35:38 -0300

    Consulta 2 feita.

--- sql/consultas.sql
+++ sql/consultas.sql
@@ -17,7 +17,9 @@ ORDER BY h.cnes_hospital;
 
 --Consulta 2
 
-SELECT *
+-- Versão 1 (Feita pelo Avatas)
+/*
+SELECT p.id_pedido
 FROM relatorio_recurso r JOIN pedido p ON r.id_pedido_relatorio = p.id_pedido
     JOIN turno t ON p.id_turno = t.id_turno
     JOIN entidade_saude es ON t.cnes_entidade_saude = es.cnes
@@ -27,3 +29,30 @@ WHERE p.quantidade > (
     JOIN recurso r2 ON p2.registro_ms_recurso = r2.registro_ms
     WHERE (es2.cnes <> es.cnes AND r2.registro_ms = p.registro_ms_recurso)
 );
+
+-- Versão 2 (Mais performance em comparação à 3 porém menos informações)
+SELECT P.id_pedido, R.nome, R.registro_ms, P.quantidade -- Pode retornar somente com o ID do recurso para evitar uma junção adicional
+    FROM PEDIDO P
+    JOIN turno T ON P.id_turno = T.id_turno 
+    JOIN entidade_saude ES ON ES.cnes = T.cnes_entidade_saude
+    JOIN recurso R ON R.registro_ms = P.registro_ms_recurso -- Linha opcional caso deseja-se listar também qual o nome do recurso em falta.
+WHERE P.quantidade > (
+    SELECT SUM(PO.quantidade_disponivel) as ESTOQUE_EXTERNO FROM possui PO 
+    JOIN entidade_saude ES2 ON PO.cnes_entidade_saude = ES2.cnes
+    WHERE ES2.cnes <> ES.cnes AND PO.registro_ms_recurso = P.registro_ms_recurso
+    GROUP BY PO.registro_ms_recurso);
+*/
+
+-- Versão 3 (Menos performance porém bem mais explicativo e útil)
+SELECT P.id_pedido, R.nome, R.registro_ms, P.quantidade, EXT.ESTOQUE_EXTERNO, (EXT.ESTOQUE_EXTERNO - P.quantidade) AS FALTA
+    FROM PEDIDO P
+    JOIN turno T ON P.id_turno = T.id_turno 
+    JOIN entidade_saude ES ON ES.cnes = T.cnes_entidade_saude
+    JOIN recurso R ON R.registro_ms = P.registro_ms_recurso 
+JOIN LATERAL(
+    SELECT SUM(PO.quantidade_disponivel) as ESTOQUE_EXTERNO 
+    FROM possui PO 
+    JOIN entidade_saude ES2 ON PO.cnes_entidade_saude = ES2.cnes
+    WHERE ES2.cnes <> ES.cnes AND PO.registro_ms_recurso = P.registro_ms_recurso
+    GROUP BY PO.registro_ms_recurso) AS EXT ON TRUE
+WHERE P.quantidade > EXT.ESTOQUE_EXTERNO;
